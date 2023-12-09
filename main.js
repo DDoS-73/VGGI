48af4cca11248fd1dd20d22af74109f43a4e9141
Vlad Pozniak <vp.9433@gmail.com>
2023-12-09 16:37:48 +0200

    pa3

--- main.js
+++ main.js
@@ -4,27 +4,28 @@ let gl;                         // The webgl context.
 let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let lightPositionEl;
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
 
 
-let horizontalLinesAmount = 0;
-let verticalLinesAmount = 0;
 // Constructor
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iNormalBuffer = gl.createBuffer();
     this.count = 0;
-    this.verticesLength = 0;
 
-    this.BufferData = function(vertices) {
-
+    this.BufferData = function(vertices, normal) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-        this.verticesLength  = vertices.length;
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normal), gl.STREAM_DRAW);
+
+        this.count = vertices.length/3;
     }
 
     this.Draw = function() {
@@ -33,15 +34,11 @@ function Model(name) {
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
 
-        // horizontal lines
-        for (let i = 0; i < horizontalLinesAmount; i++ ) {
-            gl.drawArrays(gl.LINE_STRIP, verticalLinesAmount * i, verticalLinesAmount);
-        }
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iNormal);
 
-        // vertical lines
-        for (let i = 0; i < verticalLinesAmount; i++ ) {
-            gl.drawArrays(gl.LINE_STRIP, this.verticesLength / 6 + (horizontalLinesAmount * i), horizontalLinesAmount);
-        }
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
     }
 }
 
@@ -54,10 +51,15 @@ function ShaderProgram(name, program) {
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
-    // Location of the uniform specifying a color for the primitive.
-    this.iColor = -1;
     // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
+
+    // Normals
+    this.iNormal = -1;
+    this.iNormalMatrix = -1;
+
+    // Light position
+    this.iLightPos = -1;
 
     this.Use = function() {
         gl.useProgram(this.prog);
@@ -69,12 +71,12 @@ function ShaderProgram(name, program) {
  * (Note that the use of the above drawPrimitive function is not an efficient
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
-function draw() {
+function draw(lightPos) {
     gl.clearColor(0,0,0,1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12);
+    let projection = m4.orthographic(-20, 20, -20, 20, -20, 20);
 
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
@@ -85,22 +87,68 @@ function draw() {
     let matAccum0 = m4.multiply(rotateToPointZero, modelView );
     let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
 
+    const modelviewInv = m4.inverse(matAccum1, new Float32Array(16));
+    const normalMatrix = m4.transpose(modelviewInv, new Float32Array(16));
+
     /* Multiply the projection matrix times the modelview matrix to give the
        combined transformation matrix, and send that to the shader program. */
     let modelViewProjection = m4.multiply(projection, matAccum1 );
 
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
 
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
+
+    gl.uniform3fv(shProgram.iLightPos, lightPos);
 
     surface.Draw();
 }
 
-function CreateSurfaceData()
-{
+let startX = -8;
+let startY = 20;
+let startZ = 10;
+
+let endX = -3;
+let endY = 0;
+let endZ = 10;
+
+let lightX = startX;
+let lightY = startY;
+let lightZ = startZ;
+// Крок для анімації
+let shift = 0.003;
+
+
+const step = {
+    _value: 0.01,
+    shift: 0.005,
+    set step(value) {
+        if(this._value > 1) {
+            this.shift = -shift
+        }
+        if(this._value < 0) {
+            this.shift = shift
+        }
+        this._value += this.shift;
+    },
+    get step() {
+        return this._value;
+    }
+}
+
+function anim() {
+    lightX = startX + (endX - startX) * step.step;
+    lightY = startY + (endY - startY) * Math.pow((lightX - startX) / (endX - startX), 2);
+    lightZ = startZ + (endZ - startZ) * step.step;
+
+    step.step = shift;
+    draw([lightX, lightY, lightZ])
+    window.requestAnimationFrame(anim)
+}
+
+function CreateSurfaceData() {
     let vertexList = [];
-    let scale = 0.1;
+    let normalList = [];
+    let scale = 1;
     let R2 = 4;
     let R1 = 1.3 * R2;
     const a = R2 - R1;
@@ -108,27 +156,62 @@ function CreateSurfaceData()
     const b = c;
 
     //  Surface of Conjugation of Coaxial Cylinder and Cone
+
     for (let z = 0; z < b; z += 0.5) {
         for (let beta = 0; beta <= 2 * Math.PI; beta += 0.2) {
-            const r = a * (1 - Math.cos(2 * Math.PI * z / c)) + R1;
-            const x = r * Math.cos(beta);
-            const y = r * Math.sin(beta);
-            vertexList.push(x * scale, y * scale, z * scale);
+            const p1 = createPoint(beta, z);
+            const p2 = createPoint(beta, z + 0.5);
+            const p3 = createPoint(beta + 0.2, z);
+            const p4 = createPoint(beta + 0.2, z + 0.5);
+
+            let normal1 = calculateFacetNormal(p1, p2, p3);
+            let normal2 = calculateFacetNormal(p3, p2, p4);
+            let avg = calculateAverageNormal(normal1, normal2);
+
+            vertexList.push(p1.x, p1.y, p1.z);
+            normalList.push(avg[0], avg[1], avg[2]);
+
+            vertexList.push(p2.x, p2.y, p2.z);
+            normalList.push(avg[0], avg[1], avg[2]);
+
+            vertexList.push(p3.x, p3.y, p3.z);
+            normalList.push(avg[0], avg[1], avg[2]);
+
+            vertexList.push(p2.x, p2.y, p2.z);
+            normalList.push(avg[0], avg[1], avg[2]);
+
+            vertexList.push(p3.x, p3.y, p3.z);
+            normalList.push(avg[0], avg[1], avg[2]);
+
+            vertexList.push(p4.x, p4.y, p4.z);
+            normalList.push(avg[0], avg[1], avg[2]);
         }
-        horizontalLinesAmount++;
     }
 
-    for (let beta = 0; beta <= 2 * Math.PI; beta += 0.2) {
-        for (let z = 0; z < b; z += 0.5) {
-            const r = a * (1 - Math.cos(2 * Math.PI * z / c)) + R1;
-            const x = r * Math.cos(beta);
-            const y = r * Math.sin(beta);
-            vertexList.push(x * scale, y * scale, z * scale);
-        }
-        verticalLinesAmount++;
-    }
+    return { vertices: vertexList, normal: normalList };
+}
 
-    return vertexList;
+function calculateFacetNormal(pointForNormal, p1, p2) {
+    const v1 = m4.subtractVectors([pointForNormal.x, pointForNormal.y, pointForNormal.z], [p1.x, p1.y, p1.z]);
+    const v2 = m4.subtractVectors([p2.x, p2.y, p2.z], [p1.x, p1.y, p1.z]);
+    return m4.normalize(m4.cross(v1, v2));
+}
+
+function calculateAverageNormal(n1, n2) {
+    return m4.normalize(m4.addVectors(n1, n2));
+}
+
+function createPoint(beta, z) {
+    let R2 = 4;
+    let R1 = 1.3 * R2;
+    const a = R2 - R1;
+    const c = 4 * R1;
+    const b = c;
+
+    const r = a * (1 - Math.cos(2 * Math.PI * z / c)) + R1;
+    const x = r * Math.cos(beta);
+    const y = r * Math.sin(beta);
+    return {x, y, z};
 }
 
 
@@ -141,10 +224,15 @@ function initGL() {
 
     shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
+
+    shProgram.iNormal                    = gl.getAttribLocation(prog, 'normal');
+    shProgram.iNormalMatrix              = gl.getUniformLocation(prog, 'NormalMatrix');
+
+    shProgram.iLightPos                  = gl.getUniformLocation(prog, 'lightPosition');
 
     surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData());
+    const data = CreateSurfaceData();
+    surface.BufferData(data.vertices, data.normal);
 
     gl.enable(gl.DEPTH_TEST);
 }
@@ -164,19 +252,19 @@ function createProgram(gl, vShader, fShader) {
     gl.compileShader(vsh);
     if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
         throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-     }
+    }
     let fsh = gl.createShader( gl.FRAGMENT_SHADER );
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
     if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
-       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+        throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
     gl.attachShader(prog,vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
     if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
-       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+        throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
@@ -186,6 +274,8 @@ function createProgram(gl, vShader, fShader) {
  * initialization function that will be called when the page has loaded
  */
 function init() {
+    lightPositionEl = document.getElementById('lightPostion');
+
     let canvas;
     try {
         canvas = document.getElementById("webglcanvas");
@@ -210,5 +300,5 @@ function init() {
 
     spaceball = new TrackballRotator(canvas, draw, 0);
 
-    draw();
+    window.requestAnimationFrame(anim);
 }
